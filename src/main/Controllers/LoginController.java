c3b438f011d0a55607cbf5acfaab17590e136878
Bach Nguyen <bachbet34@gmail.com>
2024-10-17 22:23:19 +0700

    Setup Project Structure

---
+++ src/main/Controllers/LoginController.java
@@ -0,0 +1,30 @@
+package main.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label loginMessage;
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("admin") && password.equals("1234")) {
+            loginMessage.setText("Login successful!");
+        } else {
+            loginMessage.setText("Invalid username or password.");
+        }
+    }
+}
